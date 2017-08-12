c3f0453d47a7608786d6f003d4263760caf86b59
David Tapson <dave.tapson@gmail.com>
2017-08-12 22:33:08 +0200

    Add tests for GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
+const todos = [
+    { text: 'First test todo' },
+    { text: 'Second test todo' },
+    { text: 'Third test todo' }
+];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -24,7 +32,7 @@ describe('POST /todos', () => {
                     return done(error);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({ text }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -33,20 +41,32 @@ describe('POST /todos', () => {
             });
     });
 
-    it('should not create todo with invalid body data', (done)=>{
-request(app)
-.post('/todos')
-.send({})
-.expect(400)
-.end((error, response)=>{
-    if(error){
-        return done(error);
-    }
+    it('should not create todo with invalid body data', (done) => {
+        request(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((error, response) => {
+                if (error) {
+                    return done(error);
+                }
 
-    Todo.find().then((todos)=>{
-        expect(todos.length).toBe(0);
-        done();
-    }).catch((e)=> done(e));
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(3);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
 });
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((response) => {
+                expect(response.body.todos.length).toBe(3);
+            })
+            .end(done);
     });
 });
