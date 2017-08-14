f285d97ab1a0322d0300e193c539244bfb0a708f
David Tapson <dave.tapson@gmail.com>
2017-08-14 21:30:32 +0200

    Added GET /todos/id

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var { mongoose } = require('./db/mongoose');
-var { Todo } = require('./models/todo');
-var { User } = require('./models/user');
+const { mongoose } = require('./db/mongoose');
+const { ObjectID } = require('mongodb');
 
-var app = express();
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+
+const app = express();
 
 app.use(bodyParser.json());
 
@@ -23,13 +25,40 @@ app.post('/todos', (request, response) => {
     });
 });
 
-app.get('/todos', (request, response)=>{
-    Todo.find().then((todos)=>{
-response.send({todos});
-    },(e)=>{
+app.get('/todos', (request, response) => {
+    Todo.find().then((todos) => {
+        response.send({ todos });
+    }, (e) => {
         response
-        .status(400)
-        .send(e);
+            .status(400)
+            .send(e);
+    });
+});
+
+// GET /todos/1234
+app.get('/todos/:id', (request, response) => {
+    var id = request.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return response
+            .status(404)
+            .send();//`User with Id: ${id} is invalid`);
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return response
+                .status(404)
+                .send();//`User with Id: ${id} not found`);
+        }
+        response
+            .status(200)
+            .send({todo});
+
+    }).catch((e) => {
+        response
+            .status(400)
+            .send();
     });
 });
 
@@ -37,4 +66,4 @@ app.listen(3000, () => {
     console.log('Started on port 3000');
 });
 
-module.exports = {app};
+module.exports = { app };
